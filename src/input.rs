ad834d73ebb3fcea0e13f334b65ccd2c62746c16
ShizukaKokoro <107260995+ShizukaKokoro@users.noreply.github.com>
2024-10-02 22:16:07 +0900

    feat(input): :sparkles: input マクロを実装

---
+++ src/input.rs
@@ -0,0 +1,70 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::parse::{ParseStream, Parser};
+use syn::{Error, Local, Pat, PatType, Result, Stmt};
+
+pub fn input_impl(tokens: TokenStream) -> TokenStream {
+    input_parse
+        .parse2(tokens)
+        .unwrap_or_else(Error::into_compile_error)
+}
+
+pub fn input_parse(input: ParseStream) -> Result<TokenStream> {
+    let mut stmts = Vec::new();
+    while !input.is_empty() {
+        let stmt: Stmt = input.parse()?;
+        match stmt {
+            Stmt::Local(Local { pat, .. }) => match pat {
+                Pat::Type(PatType { pat, ty, .. }) => {
+                    let pat = pat.clone();
+                    let ty = ty.clone();
+                    let num = stmts.len();
+                    stmts.push(quote! {
+                        let mut con = cmap.get_container(&self_.inputs()[#num]).await.unwrap();
+                        let #pat: #ty = con.take().unwrap();
+                    });
+                }
+                _ => return Err(Error::new_spanned(pat, "expected pattern type")),
+            },
+            _ => return Err(Error::new_spanned(stmt, "expected local statement")),
+        }
+    }
+    Ok(quote! { #(#stmts)* })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_input_parse() {
+        let input = quote! {
+            let arg_to0: &str;
+        };
+        let result = input_impl(input).to_string();
+        let expected = quote! {
+            let mut con = cmap.get_container(&self_.inputs()[0usize]).await.unwrap();
+            let arg_to0: &str = con.take().unwrap();
+        }
+        .to_string();
+        assert_eq!(result, expected,);
+    }
+
+    #[test]
+    fn test_input_parse_multi() {
+        let input = quote! {
+            let arg_0to1_int: i32;
+            let arg_0to1_str: &str;
+        };
+        let result = input_impl(input).to_string();
+        let expected = quote! {
+            let mut con = cmap.get_container(&self_.inputs()[0usize]).await.unwrap();
+            let arg_0to1_int: i32 = con.take().unwrap();
+            let mut con = cmap.get_container(&self_.inputs()[1usize]).await.unwrap();
+            let arg_0to1_str: &str = con.take().unwrap();
+        }
+        .to_string();
+        assert_eq!(result, expected,);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,16 @@
 //! Donburako を扱う上で便利なマクロを提供する。
 
 #![warn(missing_docs, rustdoc::missing_crate_level_docs, unused_results)]
+
+mod input;
+
+use crate::input::input_impl;
+use proc_macro::TokenStream;
+
+/// 入力をパースするマクロ
+///
+/// ユーザー定義関数に登録する関数の入力を行うコードを生成する。
+#[proc_macro]
+pub fn input(tokens: TokenStream) -> TokenStream {
+    input_impl(tokens.into()).into()
+}
