ad06c689817b75b10960d84a1041de937d0f0726
ShizukaKokoro <107260995+ShizukaKokoro@users.noreply.github.com>
2024-10-02 22:32:46 +0900

    feat(output): :sparkles: output マクロを実装

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@
 #![warn(missing_docs, rustdoc::missing_crate_level_docs, unused_results)]
 
 mod input;
+mod output;
 
 use crate::input::input_impl;
+use crate::output::output_impl;
 use proc_macro::TokenStream;
 
 /// 入力をパースするマクロ
@@ -15,4 +17,12 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn input(tokens: TokenStream) -> TokenStream {
     input_impl(tokens.into()).into()
+}
+
+/// 出力をパースするマクロ
+///
+/// ユーザー定義関数に登録する関数の出力を行うコードを生成する。
+#[proc_macro]
+pub fn output(tokens: TokenStream) -> TokenStream {
+    output_impl(tokens.into()).into()
 }
---
+++ src/output.rs
@@ -0,0 +1,82 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::parse::{ParseStream, Parser};
+use syn::{Error, Expr, Result};
+
+pub fn output_impl(tokens: TokenStream) -> TokenStream {
+    output_parse
+        .parse2(tokens)
+        .unwrap_or_else(Error::into_compile_error)
+}
+
+pub fn output_parse(input: ParseStream) -> Result<TokenStream> {
+    let mut stmts = Vec::new();
+    while !input.is_empty() {
+        let stmt: Expr = input.parse()?;
+        let num = stmts.len();
+        stmts.push(quote! {
+            let mut con_clone = con.clone_container().unwrap();
+            con_clone.store(#stmt);
+            cmap.add_container(self_.outputs()[#num].clone(), con_clone)
+                .await
+                .unwrap();
+        });
+        let _ = input.parse::<syn::Token![,]>();
+    }
+    Ok(quote! { #(#stmts)* })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_output_parse() {
+        let input = quote! {
+            "1 -> 3"
+        };
+        let result = output_impl(input).to_string();
+        let expected = quote! {
+            let mut con_clone = con.clone_container().unwrap();
+            con_clone.store("1 -> 3");
+            cmap.add_container(self_.outputs()[0usize].clone(), con_clone)
+                .await
+                .unwrap();
+        }
+        .to_string();
+        assert_eq!(result, expected,);
+    }
+
+    #[test]
+    fn test_output_parse_multi() {
+        let input = quote! {
+            42, "0 -> 1", "0 -> 2", "0 -> 3"
+        };
+        let result = output_impl(input).to_string();
+        let expected = quote! {
+            let mut con_clone = con.clone_container().unwrap();
+                con_clone.store(42);
+                cmap.add_container(self_.outputs()[0usize].clone(), con_clone)
+                    .await
+                    .unwrap();
+                let mut con_clone = con.clone_container().unwrap();
+                con_clone.store("0 -> 1");
+                cmap.add_container(self_.outputs()[1usize].clone(), con_clone)
+                    .await
+                    .unwrap();
+                let mut con_clone = con.clone_container().unwrap();
+                con_clone.store("0 -> 2");
+                cmap.add_container(self_.outputs()[2usize].clone(), con_clone)
+                    .await
+                    .unwrap();
+                let mut con_clone = con.clone_container().unwrap();
+                con_clone.store("0 -> 3");
+                cmap.add_container(self_.outputs()[3usize].clone(), con_clone)
+                    .await
+                    .unwrap();
+        }
+        .to_string();
+        assert_eq!(result, expected,);
+    }
+}
