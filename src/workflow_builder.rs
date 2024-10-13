35ffa74f466a4959cb59699420bcb2f803bff83a
ShizukaKokoro <107260995+ShizukaKokoro@users.noreply.github.com>
2024-10-13 15:11:55 +0900

    feat(workflow_builder): :construction: ノードを操作するよう実装

--- src/workflow_builder.rs
+++ src/workflow_builder.rs
@@ -1,7 +1,159 @@
+use convert_case::{Case, Casing};
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::parse::{ParseStream, Parser};
+use syn::spanned::Spanned;
+use syn::visit::Visit;
 use syn::{Error, Result};
+
+#[derive(Debug, Default)]
+struct StmtVisitor {
+    pub err: Option<Error>,
+    pub node_var_let: Vec<TokenStream>,
+    pub add_nodes: Vec<TokenStream>,
+    pub branch_cnt: usize,
+    pub select_cnt: usize,
+}
+impl StmtVisitor {
+    fn inner_visit_stmt(&mut self, stmts: &[syn::Stmt]) {
+        for (i, stmt) in stmts.iter().enumerate() {
+            if i == stmts.len() - 1 {
+                match stmt {
+                    syn::Stmt::Local(syn::Local {
+                        init: Some(local_init),
+                        ..
+                    }) => {
+                        self.visit_local_init(local_init);
+                    }
+                    syn::Stmt::Expr(expr, _) => {}
+                    _ => {
+                        self.err = Some(Error::new(stmt.span(), "Invalid statement"));
+                    }
+                }
+            } else {
+                self.visit_stmt(stmt);
+            }
+        }
+    }
+}
+impl<'ast> Visit<'ast> for StmtVisitor {
+    fn visit_expr_call(&mut self, expr_call: &'ast syn::ExprCall) {
+        let mut path = match expr_call.func.as_ref() {
+            syn::Expr::Path(syn::ExprPath { path, .. }) => path.clone(),
+            _ => {
+                self.err = Some(Error::new(expr_call.span(), "Invalid function call"));
+                return;
+            }
+        };
+        let node_var = syn::Ident::new(
+            &format!("node_{}", path.segments.last().unwrap().ident),
+            path.segments.last().unwrap().ident.span(),
+        );
+        let name = path.segments.last().unwrap().ident.to_string();
+        let builder_name = syn::Ident::new(
+            &format!("{}Builder", name.to_string().to_case(Case::Pascal)),
+            name.span(),
+        );
+        path.segments.last_mut().unwrap().ident = builder_name;
+        self.node_var_let.push(quote! {
+            let #node_var = #path::new();
+        });
+        self.add_nodes.push(quote! {
+            .add_node(#node_var)?
+        });
+    }
+
+    fn visit_expr_if(&mut self, expr_if: &'ast syn::ExprIf) {
+        let branch_name =
+            syn::Ident::new(&format!("node_branch_{}", self.branch_cnt), expr_if.span());
+        self.node_var_let.push(quote! {
+            let #branch_name = branch_builder!();
+        });
+        self.add_nodes.push(quote! {
+            .add_node(#branch_name)?
+        });
+        self.branch_cnt += 1;
+        let syn::ExprIf {
+            cond,
+            then_branch,
+            else_branch,
+            ..
+        } = expr_if;
+        self.inner_visit_stmt(&then_branch.stmts);
+        if let Some((_, else_expr)) = else_branch {
+            match else_expr.as_ref() {
+                syn::Expr::Block(block) => {
+                    self.inner_visit_stmt(&block.block.stmts);
+                }
+                _ => {
+                    self.err = Some(Error::new(else_expr.span(), "Invalid expression"));
+                }
+            }
+        }
+    }
+
+    fn visit_local_init(&mut self, local_init: &'ast syn::LocalInit) {
+        match local_init.expr.as_ref() {
+            syn::Expr::Await(expr_await) => match expr_await.base.as_ref() {
+                syn::Expr::Call(expr_call) => {
+                    self.visit_expr_call(expr_call);
+                }
+                _ => {
+                    self.err = Some(Error::new(expr_await.base.span(), "Invalid expression"));
+                }
+            },
+            syn::Expr::Call(expr_call) => {
+                self.visit_expr_call(expr_call);
+            }
+            syn::Expr::If(expr_if) => {
+                self.visit_expr_if(expr_if);
+            }
+            _ => {
+                self.err = Some(Error::new(local_init.expr.span(), "Invalid expression"));
+            }
+        }
+    }
+
+    fn visit_stmt(&mut self, stmt: &'ast syn::Stmt) {
+        match stmt {
+            syn::Stmt::Local(syn::Local {
+                pat,
+                init: Some(local_init),
+                ..
+            }) => {
+                if let syn::Expr::If(_) = local_init.expr.as_ref() {
+                    let ty = match pat {
+                        syn::Pat::Type(syn::PatType { ty, .. }) => ty.clone(),
+                        _ => {
+                            self.err = Some(Error::new(pat.span(), "Invalid pattern"));
+                            return;
+                        }
+                    };
+                    let select_name = syn::Ident::new(
+                        &format!("node_select_{}", self.select_cnt),
+                        local_init.expr.span(),
+                    );
+                    self.node_var_let.push(quote! {
+                        let #select_name = select_builder!(#ty);
+                    });
+                    self.add_nodes.push(quote! {
+                        .add_node(#select_name)?
+                    });
+                }
+                self.visit_local_init(local_init);
+            }
+            syn::Stmt::Expr(
+                syn::Expr::Return(syn::ExprReturn {
+                    expr: Some(expr), ..
+                }),
+                _,
+            ) => {}
+            _ => {
+                self.err = Some(Error::new(stmt.span(), "Invalid statement"));
+            }
+        }
+    }
+}
 
 pub fn workflow_builder_impl(_: TokenStream, tokens: TokenStream) -> TokenStream {
     workflow_builder_parse
@@ -24,7 +176,15 @@ pub fn workflow_builder_parse(input: ParseStream) -> Result<TokenStream> {
         panic!("Invalid function argument");
     }));
 
-    let node_names: Vec<syn::Ident> = Vec::new();
+    let mut visitor = StmtVisitor::default();
+    visitor.visit_block(&func.block);
+    if let Some(err) = visitor.err {
+        return Err(err);
+    }
+
+    let node_var_let = visitor.node_var_let;
+    let add_nodes = visitor.add_nodes;
+
     let (input_edge_exprs, input_edges) = {
         let mut input_edge_exprs = Vec::new();
         let mut input_edges = Vec::new();
@@ -39,21 +199,6 @@ pub fn workflow_builder_parse(input: ParseStream) -> Result<TokenStream> {
     };
     let output_edges: Vec<syn::Ident> = Vec::new();
 
-    let (node_var_names, add_nodes) = {
-        let mut node_var_names = Vec::new();
-        let mut add_nodes = Vec::new();
-        for name in node_names.iter() {
-            let builder_name = syn::Ident::new(&format!("{}Builder", name), name.span());
-            let node_name = syn::Ident::new(&format!("node_{}", name), name.span());
-            node_var_names.push(quote! {
-                let #node_name = #builder_name::new();
-            });
-            add_nodes.push(quote! {
-                .add_node(#node_name)?
-            });
-        }
-        (node_var_names, add_nodes)
-    };
     Ok(quote! {
         fn #func_name_workflow() -> Result<
             (
@@ -65,7 +210,7 @@ pub fn workflow_builder_parse(input: ParseStream) -> Result<TokenStream> {
             Box<dyn std::error::Error>,
         > {
             let wf_id = WorkflowId::new(#func_name_str);
-            #(#node_var_names)*
+            #(#node_var_let)*
             #(#input_edge_exprs)*
 
             let builder = WorkflowBuilder::default()
@@ -85,9 +230,9 @@ mod tests {
     #[test]
     fn test_workflow_builder_impl() {
         let input = quote! {
-            fn func_map(n: i32) -> Option<i32> {
-                let (n0, n1) = divide2(n);
-                let even: bool = is_even(n0);
+            async fn func_map(n: i32) -> Option<i32> {
+                let (n0, n1): (i32, i32) = divide2(n).await;
+                let even: bool = some::is_even(n0);
                 let selected: Option<i32> = if even {
                     let res: Option<i32> = double(n1);
                     res
@@ -112,42 +257,42 @@ mod tests {
                 let wf_id = WorkflowId::new("func_map");
 
                 let node_divide2 = Divide2Builder::new();
-                let node_is_even = IsEvenBuilder::new();
+                let node_is_even = some::IsEvenBuilder::new();
+                let node_select_0 = select_builder!(Option<i32>);
+                let node_branch_0 = branch_builder!();
                 let node_double = DoubleBuilder::new();
                 let node_none = NoneBuilder::new();
-                let node_branch = branch_builder!();
-                let node_select = select_builder!(Option<i32>);
 
                 let edge_n = Arc::new(Edge::new::<i32>());
                 let edge_n0 = node_divide2.outputs()[0].clone();
                 let edge_n1 = node_divide2.outputs()[1].clone();
                 let edge_even = node_is_even.outputs()[0].clone();
-                let edge_true = node_branch.outputs()[0].clone();
-                let edge_false = node_branch.outputs()[1].clone();
+                let edge_true = node_branch_0.outputs()[0].clone();
+                let edge_false = node_branch_0.outputs()[1].clone();
                 let edge_double = node_double.outputs()[0].clone();
                 let edge_none = node_none.outputs()[0].clone();
-                let edge_select = node_select.outputs()[0].clone();
+                let edge_select = node_select_0.outputs()[0].clone();
 
                 let node_divide2 = node_divide2.build(vec![edge_n.clone()], 0)?;
                 let node_is_even = node_is_even.build(vec![edge_n0.clone()], 0)?;
-                let node_branch = node_branch.build(vec![edge_even.clone()], 0)?;
+                let node_select_0 = node_select_0.build(vec![edge_double.clone(), edge_none.clone()], 0)?;
+                let node_branch_0 = node_branch_0.build(vec![edge_even.clone()], 0)?;
                 let node_double = node_double.build(vec![edge_true.clone(), edge_n1.clone()], 1)?;
                 let node_none = node_none.build(vec![edge_false.clone()], 1)?;
-                let node_select = node_select.build(vec![edge_double.clone(), edge_none.clone()], 0)?;
 
                 let builder = WorkflowBuilder::default()
                     .add_node(node_divide2)?
                     .add_node(node_is_even)?
-                    .add_node(node_branch)?
+                    .add_node(node_select_0)?
+                    .add_node(node_branch_0)?
                     .add_node(node_double)?
-                    .add_node(node_none)?
-                    .add_node(node_select)?;
+                    .add_node(node_none)?;
 
                 Ok((wf_id, builder, vec![edge_n], vec![edge_select]))
             }
-            fn func_map(n: i32) -> Option<i32> {
-                let (n0, n1) = divide2(n);
-                let even: bool = is_even(n0);
+            async fn func_map(n: i32) -> Option<i32> {
+                let (n0, n1): (i32, i32) = divide2(n).await;
+                let even: bool = some::is_even(n0);
                 let selected: Option<i32> = if even {
                     let res: Option<i32> = double(n1);
                     res
